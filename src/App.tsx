031e67c7f457af9149061f93f8eb4a0e55498b21
juanbuenoo1988 <jj.bueno@montajesdelsaz.com>
2025-10-15 18:18:09 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "4rem", color: "white" }}>
+      <h2>¡Hola! La aplicación funciona correctamente ✅</h2>
+      <p>Puedes modificar este texto más adelante.</p>
+    </div>
+  );
+}
