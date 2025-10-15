65439fd6e53a3e16c46b9be64a01434187d8ecdb
juanbuenoo1988 <jj.bueno@montajesdelsaz.com>
2025-10-15 21:54:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import type { Session } from "@supabase/supabase-js";
+import { supabase } from "./lib/supabase"; // o "../lib/supabase" según tengas la ruta
 import Planificador from "./Planificador";
 
 export default function App() {
