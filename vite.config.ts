00e26ca9d626ef40f30c257983e0fac115cae4d6
juanbuenoo1988 <jj.bueno@montajesdelsaz.com>
2025-10-17 20:22:52 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false,     // ⚠️ solo temporal para ver el error claro
+    sourcemap: true,
+    target: 'esnext'
+  },
+  optimizeDeps: {
+    include: ['date-fns', 'date-fns/locale/es']
+  }
 });
